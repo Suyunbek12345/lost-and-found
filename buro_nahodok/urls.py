5a9aed69ab2c27265199f6c8009be68755e18668
Suyunbek12345 <bekkeldievsuiun18@gmail.com>
2022-12-05 00:53:03 +0600

    news

--- buro_nahodok/urls.py
+++ buro_nahodok/urls.py
@@ -1,20 +1,40 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import SimpleRouter
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 from category.views import CategoryViewSet
 
 router = SimpleRouter()
 router.register('categories', CategoryViewSet)
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api-auth', include('rest_framework.urls')),
     path('api/v1/accounts/', include('account.urls')),
     path('api/v1/table/', include('table.urls')),
-    # path('api/v1/category', include('catego')),
+    # path('api/v1/category', include('category')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

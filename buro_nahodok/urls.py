3650c3dfb2299295c63b00f6881d7834af2365e0
Suyunbek12345 <bekkeldievsuiun18@gmail.com>
2022-12-06 13:16:56 +0600

    ..

--- buro_nahodok/urls.py
+++ buro_nahodok/urls.py
@@ -27,9 +27,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('doc', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api-auth', include('rest_framework.urls')),
     path('api/v1/', include(router.urls)),
@@ -39,3 +37,4 @@ urlpatterns = [
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
